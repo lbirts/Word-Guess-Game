a0892be449652354551dc1782ca568476737f8e6
Lauren Birts <birtslauren@gmail.com>
2020-02-05 22:00:18 -0500

    assigning vars

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //Global
+    var wordList = ["pencil", "calculator", "notebook", "pen", "binder", "paper", "textbook", "laptop", "highlighter", "planner", "flashcards"];
+    var guessedLetters = [];
+    //Counters
+    var wins = 0;
+    var losses = 0;
+    var numGuesses = 10;
+
+
+
+})
